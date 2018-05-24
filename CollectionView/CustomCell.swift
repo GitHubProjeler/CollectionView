41781c4e5a1a9e3191b194dfc0f457bbc8b76bc2
fatih acar <fatihacar@fatih-mac.local>
2018-05-24 11:06:30 +0300

    günceleme

---
+++ CollectionView/CustomCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomCell.swift
+//  CollectionView
+//
+//  Created by fatih acar on 23.05.2018.
+//  Copyright © 2018 fatih acar. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UICollectionViewCell {
+    
+    @IBOutlet weak var resim: UIImageView!
+    
+}
--- CollectionView/ViewController.swift
+++ CollectionView/ViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    var resimler = ["1","2","3","4","5"]
+    @IBOutlet weak var btnRenk: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //CollectionView metotları
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return resimler.count
     }
-
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "myCell", for: indexPath) as! CustomCell //CustomCell sınıfından alınacak
+        cell.resim.image = UIImage(named:resimler[indexPath.row])
+        cell.layer.cornerRadius = 50
+        cell.layer.borderColor = UIColor.red.cgColor
+        cell.layer.borderWidth = 2
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("\(indexPath.row + 1). resim tıklandı")
+        if (indexPath.row == 0){
+            btnRenk.backgroundColor = UIColor.black
+        } else if (indexPath.row == 1){
+            btnRenk.backgroundColor = UIColor.blue
+        } else if (indexPath.row == 2){
+            btnRenk.backgroundColor = UIColor.brown
+        } else if (indexPath.row == 3){
+            btnRenk.backgroundColor = UIColor.cyan
+        }
+    }
 }
 
